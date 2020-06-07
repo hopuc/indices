39904e239d19836bffbd693cfd090834f2249552
Ho Yeung <mail@hopuc.com>
2020-06-08 00:31:00 +0800

    update

--- src/pages/Chart.vue
+++ src/pages/Chart.vue
@@ -21,6 +21,7 @@
   import echarts from 'echarts/lib/echarts'
   import 'echarts/lib/chart/line'
   import 'echarts/lib/component/title'
+  import 'echarts/lib/component/tooltip'
   import 'echarts/lib/component/markLine'
   export default {
     data(){
